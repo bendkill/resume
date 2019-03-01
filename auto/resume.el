86d206ba19834e5aca8e46360507928e852b01a5
killeen <killeen@uchicago.edu>
2019-03-01 16:50:02 -0600

    updated with grader position

--- auto/resume.el
+++ auto/resume.el
@@ -2,20 +2,20 @@
  "resume"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("article" "letterpaper" "12pt")))
+                     '(("article" "letterpaper" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("fullpage" "empty") ("color" "usenames" "dvipsnames") ("hyperref" "pdftex")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
-    "art12"
+    "art11"
     "latexsym"
     "fullpage"
     "titlesec"
